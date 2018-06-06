abdb5c55564a5d6feb1776a905c82829ad77e116
Rajni <rajnikant12345@gmail.com>
2018-06-06 14:40:26 +0530

    Update connection_test.go

--- chatserver/cmd/connection_test.go
+++ chatserver/cmd/connection_test.go
@@ -7,14 +7,20 @@ import (
 	"net"
 	"encoding/json"
 	"fmt"
-
 	"cryptolessons/chatserver/processors"
 
+	"sync"
+	"time"
 )
 
-func startserver() {
-	os.Setenv("APP_CFG", "/Users/rajnikant/workspace/src/cryptolessons/app.json")
-	go StartServer()
+
+func Decoder(c net.Conn) {
+	dec := json.NewDecoder(c)
+	for {
+		m := model.CommonMessage{}
+		dec.Decode(&m)
+		fmt.Println(m.Msg.From , m.Msg.To , m.Msg.Data)
+	}
 }
 
 
@@ -22,15 +28,15 @@ func startserver() {
 
 func TestHandleConnections(t *testing.T) {
 
-	users := make([]string,100)
+	users := make([]string,200)
 	cid := make([]string,10000)
 	conn := make([]net.Conn,10000)
-//	var wg sync.WaitGroup
-	//wg.Add(100)
+	var wg sync.WaitGroup
+
 	for i,_ := range users {
 
-
-		func(i int) {
+		//wg.Add(1)
+		 func(i int) {
 			//defer wg.Done()
 			users[i] = processors.GenerateConnectinId()
 			c,e := net.Dial("tcp","localhost:6789")
@@ -50,46 +56,37 @@ func TestHandleConnections(t *testing.T) {
 			m.Lgin = &model.Login{}
 			m.Lgin.UserName = users[i]
 			enc.Encode(&m)
-			dec.Decode(&m)
-
+			go Decoder(c)
 		}(i)
 	}
 
-
-
-	//wg.Wait()
-
-	//wg.Add(10000)
-
 	for i,_ := range users {
+
 		for j,_ := range users {
-			//wg.Add(1)
-			func(i , j int) {
-				//defer wg.Done()
-				//fmt.Println(i,j)
+			wg.Add(1)
+			 go func(i , j int) {
+				defer wg.Done()
 				if i == j {
 					return
 				}
 				enc := json.NewEncoder(conn[i])
 				m := model.CommonMessage{}
 				m.Conn = cid[i]
-				//fmt.Println(cid[i])
 				m.Msg = &model.Message{}
-				m.Msg.Data = "hello"
+				m.Msg.Data = "hello " + users[j] + " from " + users[i]
 				m.Msg.From = users[i]
 				m.Msg.To = users[j]
-				//mm,_ :=  json.Marshal(&m)
-				//fmt.Println(string(mm))
 				enc.Encode(&m)
-				dec := json.NewDecoder(conn[j])
-				dec.Decode(&m)
-				//fmt.Println(j)
 			}(i,j)
 
 		}
 
 	}
+	wg.Wait()
 
-	//wg.Wait()
+	for i,_ := range users {
+		conn[i].Close()
+	}
+
 
 }
